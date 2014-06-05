c99b6cc99c3922181497d92a75b5a1677140e4bf
marcosArruda <marcos.f.arruda@gmail.com>
2014-06-05 01:05:38 -0300

    Cleaned and first test communication with arduino via USB serial port.

--- src/main/java/br/com/rpiautom/GPIOServiceController.java
+++ src/main/java/br/com/rpiautom/GPIOServiceController.java
@@ -3,17 +3,7 @@ package br.com.rpiautom;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import br.com.rpiautom.gpio.CommandExecutor;
-import br.com.rpiautom.gpio.PinMapper;
-
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
 
 /**
  * Handles requests to the gpio.
@@ -24,7 +14,7 @@ public class GPIOServiceController {
 	
 	// create gpio controller instance
 	//TODO: Injetar isso.
-	final GpioController gpio = GpioFactory.getInstance();
+	//final GpioController gpio = GpioFactory.getInstance();
 	
 	@RequestMapping(value = "/led-toggle", method = RequestMethod.GET)
 	@ResponseBody
@@ -32,8 +22,8 @@ public class GPIOServiceController {
 		// URI : /gpio/activate
 		
 		// provision gpio pins #04 as an output pin and make sure is is set to LOW at startup
-		GpioPinDigitalOutput myLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04,   // PIN NUMBER
-		                                                           "My LED");           // PIN FRIENDLY NAME (optional)
+		//GpioPinDigitalOutput myLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04,   // PIN NUMBER
+		//                                                           "My LED");           // PIN FRIENDLY NAME (optional)
 		 // explicitly set a state on the pin object
         //myLed.setState(PinState.HIGH);
 
@@ -42,12 +32,14 @@ public class GPIOServiceController {
         //myLed.high();
 
         // use toggle method to apply inverse state on the pin object
-        myLed.toggle();
+        //myLed.toggle();
 
         // use pulse method to set the pin to the HIGH state for
         // an explicit length of time in milliseconds
         //myLed.pulse(1000);
-        
+		//import com.pi4j.wiringpi.Serial;
+		//Serial.serialOpen(arg0, arg1)
         return "OK";
+        
 	}
 }
