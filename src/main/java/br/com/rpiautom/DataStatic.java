6794ac55f2fc8c839ff04a4f906b7c444d0b3c08
marcosArruda <marcos.f.arruda@gmail.com>
2014-06-10 03:39:39 -0300

    added plain use of the pi4j: 	- Service to control a pin (on, off) 	- Service to send message to arduino via serial.

---
+++ src/main/java/br/com/rpiautom/DataStatic.java
@@ -0,0 +1,5 @@
+package br.com.rpiautom;
+
+public class DataStatic {
+	public static String lastArduinoResponse = "Nothing...";
+}
--- src/main/java/br/com/rpiautom/GPIOServiceController.java
+++ src/main/java/br/com/rpiautom/GPIOServiceController.java
@@ -1,45 +1,113 @@
 package br.com.rpiautom;
 
+import java.util.Collection;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPin;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.serial.Serial;
+import com.pi4j.io.serial.SerialDataEvent;
+import com.pi4j.io.serial.SerialDataListener;
+import com.pi4j.io.serial.SerialFactory;
+import com.pi4j.io.serial.SerialPortException;
+
 /**
- * Handles requests to the gpio.
+ * Handles requests to the gpio. /gpio/led-on.html /gpio/led-off.html
  */
 @Controller
 @RequestMapping("/gpio")
 public class GPIOServiceController {
-	
 	// create gpio controller instance
-	//TODO: Injetar isso.
-	//final GpioController gpio = GpioFactory.getInstance();
-	
-	@RequestMapping(value = "/led-toggle", method = RequestMethod.GET)
+	// TODO: Injetar isso.
+
+	static final GpioController gpio = GpioFactory.getInstance();
+
+	@RequestMapping(value = "/led-on", method = RequestMethod.GET)
 	@ResponseBody
-	public String activate() {
-		// URI : /gpio/activate
-		
-		// provision gpio pins #04 as an output pin and make sure is is set to LOW at startup
-		//GpioPinDigitalOutput myLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04,   // PIN NUMBER
-		//                                                           "My LED");           // PIN FRIENDLY NAME (optional)
-		 // explicitly set a state on the pin object
-        //myLed.setState(PinState.HIGH);
+	public String ledOn() {
+		// explicitly set a state on the pin object
+		// myLed.setState(PinState.HIGH);
+		// use convenience wrapper method to set state on the pin object
+		// myLed.low();
+		// myLed.pulse(3000);
+		// use toggle method to apply inverse state on the pin object
+		// myLed.toggle();
+		// use pulse method to set the pin to the HIGH state for
+		// an explicit length of time in milliseconds
+		// myLed.pulse(1000);
+		// import com.pi4j.wiringpi.Serial;
+		// Serial.serialOpen(arg0, arg1)
+		// SerialFactory.createInstance().open(arg0, arg1);
+		GpioPinDigitalOutput myLed = getLed();
+		myLed.high();
+		return "led ON";
+	}
 
-        // use convenience wrapper method to set state on the pin object
-        //myLed.low();
-        //myLed.high();
+	@RequestMapping(value = "/led-off", method = RequestMethod.GET)
+	@ResponseBody
+	public String ledOff() {
+		GpioPinDigitalOutput myLed = getLed();
+		myLed.low();
+		return "led OFF";
+	}
+	
+	@RequestMapping(value = "/arduino0", method = RequestMethod.GET)
+	@ResponseBody
+	public String getArduino0Response() {
+		return DataStatic.lastArduinoResponse;
+	}
 
-        // use toggle method to apply inverse state on the pin object
-        //myLed.toggle();
+	@RequestMapping(value = "/hello-arduino0", method = RequestMethod.GET)
+	@ResponseBody
+	public String sendHelloToArduino0() {
 
-        // use pulse method to set the pin to the HIGH state for
-        // an explicit length of time in milliseconds
-        //myLed.pulse(1000);
-		//import com.pi4j.wiringpi.Serial;
-		//Serial.serialOpen(arg0, arg1)
-        return "OK";
-        
+		final Serial serial = SerialFactory.createInstance();
+
+		// create and register the serial data listener
+		serial.addListener(new SerialDataListener() {
+			@Override
+			public void dataReceived(SerialDataEvent event) {
+				// print out the data received to the console
+				System.out.print(event.getData());
+				DataStatic.lastArduinoResponse = event.getData();
+				ledOn();
+			}
+		});
+
+		try {
+			serial.open("/dev/ttyUSB0", 38400);
+			try {
+				serial.write("Ola Arduino");
+			} catch (IllegalStateException ex) {
+				ex.printStackTrace();
+				return " ==>> SERIAL SETUP SUCCESS BUT...: " + ex.getMessage();
+			}
+		} catch (SerialPortException ex) {
+			System.out.println();
+			return " ==>> SERIAL SETUP FAILED : " + ex.getMessage();
+		}
+		return " ==>> SERIAL SETUP SUCCESS AND MESSAGE SEND!";
+	}
+
+	/**
+	 * RaspiPin.GPIO_05 = pin 18 GPIO_24
+	 */
+	private GpioPinDigitalOutput getLed() {
+		Collection<GpioPin> gpioPins = gpio.getProvisionedPins();
+		GpioPinDigitalOutput myLed;
+		if (gpioPins.isEmpty()) {
+			myLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "My LED5");
+		} else {
+			myLed = (GpioPinDigitalOutput) gpioPins.iterator().next();
+		}
+		return myLed;
 	}
 }
